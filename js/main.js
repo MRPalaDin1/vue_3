b947fc3d76488cbe77f386acb0ca4ecd6bc03818
MRPalaDin1 <mrpaladin228322954@gmail.com>
2024-02-08 22:48:10 +0700

    fourth

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ Vue.component('kanban-board', {
     <div>
       <header>
           <h1 class="title">Kanban</h1>
+          <input class="input" type="text" v-model="currentNote"  @keyup.enter="addNote">
       </header>
-      <input class="input" type="text" v-model="currentNote"  @keyup.enter="addNote">
       <main>
-          <div class="all_columns">
-              <div class="column column1">
-                  <h4 class="column-title">Planned</h4>
+          <div class="columns-container">
+            <div class="column">
+                <h4 class="column-title">Запланированные задачи</h4>
                   <ul class="list" v-for="note in notes">
                       <li class="cart" v-if="note.isPlanned === true">
                           <p>{{note.text}}</p>
@@ -19,8 +19,8 @@ Vue.component('kanban-board', {
                   </ul>
               </div>
 
-              <div class="column column2">
-                  <h4 class="column-title">In process</h4>
+              <div class="column">
+                <h4 class="column-title">Задачи в работе</h4>
                   <ul class="list" v-for="note in notes">
                       <li class="cart" v-if="note.isProcess === true">
                           <p>{{note.text}}</p>
@@ -33,8 +33,8 @@ Vue.component('kanban-board', {
                   </ul>
               </div>
 
-              <div class="column column3">
-                  <h4 class="column-title">Testing</h4>
+              <div class="column">
+                <h4 class="column-title">Тестирование</h4>
                   <ul class="list" v-for="note in notes">
                       <li class="cart" v-if="note.isTested === true">
                           <p>{{note.text}}</p>
@@ -46,8 +46,8 @@ Vue.component('kanban-board', {
                   </ul>
               </div>
 
-              <div class="column column4">
-                  <h4 class="column-title">Done</h4>
+              <div class="column">
+                <h4 class="column-title">Выполненные задачи</h4>
                   <ul class="list" v-for="note in notes">
                       <li class="cart" v-if="note.isDone === true">
                           <p>{{note.text}}</p>
